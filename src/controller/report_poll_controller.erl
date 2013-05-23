f40e1a6524f3a2620405c5bfb4bee742a2bbda6a
dongaihua <744903204@qq.com>
2013-05-23 17:18:30 +0800

    first commit

---
+++ src/controller/report_poll_controller.erl
@@ -0,0 +1,7 @@
+-module(report_poll_controller, [Req, SessionId]).
+-compile(export_all).
+
+getUpdate('GET', [LastTimestamp]) ->
+	{ok, Timestamp, Objects} = boss_mq:pull("updates", list_to_integer(LastTimestamp)),
+	{json, [{timestamp, Timestamp}, {objects, Objects}]}.
+
